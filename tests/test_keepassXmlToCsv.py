8d1a930e31d1d7ec229cc89ce4101c0d9af7b845
Jean Pierre Wenzel <jeanpierre.wenzel@gmail.com>
2021-01-16 12:21:08 +0100

    Added unit testing stub

---
+++ tests/test_keepassXmlToCsv.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestKeepassXmlToCsv(unittest.TestCase):
+
+    def test_validateInputFile(self):
+        self.fail('Implement me.')
+
+if __name__ == '__main__':
+    unittest.main()
